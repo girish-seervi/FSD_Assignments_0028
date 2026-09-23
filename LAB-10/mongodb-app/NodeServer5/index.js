d3e653d9b3a8b11201552fe4d3fad61b9fb7ca96
girish-seervi <girishkailashbca24@rvu.edu.in>
2026-09-23 10:13:57 +0530

    Fix index.js, add data script, and add screenshots

--- LAB-10/mongodb-app/NodeServer5/index.js
+++ LAB-10/mongodb-app/NodeServer5/index.js
@@ -1,26 +1,42 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+require('dotenv').config()
+
 
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('view engine', 'ejs');
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/test')
-    .then(() => {
+app.set('view engine', 'ejs'); // Set EJS as the view engine    
+
+
+mongoose.
+    connect(process.env.MONGODB_URL).
+    then(() => {
         console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
+    }).catch((err) => {
         console.error('Error connecting to MongoDB:', err);
     });
- 
+
+const User= mongoose.model('User', {
+    firstName:String,
+    lastName:String,
+    email:String,
+    Phone:Number,
+});
+const Child= mongoose.model('Child', {
+    firstName:String,
+    lastName:String,
+    email:String,
+    Phone:Number,
+});
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
 });
+const PORT = process.env.PORT;
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000 ');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}  at http://localhost:${PORT}`);
 });
